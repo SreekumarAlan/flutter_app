1e9249f67631865b2519a63fbe6c699604dc2d52
371304 <371304@LT001317-371304>
2023-09-07 15:26:27 +0530

    override n super keywords

---
+++ lib/material_comp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: CheckboxExampleApp(),
+  ));
+}
+
+class CheckboxExampleApp extends StatelessWidget {
+  const CheckboxExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
