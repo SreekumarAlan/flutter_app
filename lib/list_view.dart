b0d1a3e74b5b342a4c2bacf3533cc473085ef7a6
371304 <371304@LT001317-371304>
2023-09-07 17:03:32 +0530

    parent managing state of its child widget

---
+++ lib/list_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Basic List"),
+        ),
+        body: ListView(
+          children: const<Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
