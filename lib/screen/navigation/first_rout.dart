331c3a17cb6847ae5b0ecea056df54baccf10a89
371304 <371304@LT001317-371304>
2023-09-08 15:07:26 +0530

    navigating to another page and back

---
+++ lib/screen/navigation/first_rout.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screen/navigation/second_route.dart';
+
+void main() => runApp(MaterialApp(
+  home: FirstRoute(),
+));
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First route')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=> SecondRoute()));
+          },
+          child: Text('open route'),
+        ),
+      ),
+    );
+  }
+}
